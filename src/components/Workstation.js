67a709a2310f515f02f3f5556320c046f056ba36
Aleksandar P <aleksandar.p@delasport.com>
2023-03-26 09:11:32 +0300

    restored booking sizes

--- src/components/Workstation.js
+++ src/components/Workstation.js
@@ -5,8 +5,8 @@ function Workstation({isLeft, workstation}) {
     <div className="workstation d-flex align-items-center">
         <div className="chair"></div>
         <div className="desk d-flex align-items-center justify-content-center">
-            <div className={`user-placeholder ${workstation?.user ? 'occupied' : ''}`}>
-                {workstation?.user && <div className="user-abbreviation">
+            <div className={`d-flex align-items-center justify-content-center user-placeholder ${workstation?.user ? 'occupied' : ''}`}>
+                {workstation?.user && <div className="user-abbreviation ">
                     {workstation?.user}
                 </div>}
             </div>
